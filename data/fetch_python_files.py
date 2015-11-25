77398568a4d656831868005df6c668376152919b
Jake Barnwell <jb16@mit.edu>
2015-11-25 00:46:40 -0500

    Improve fetch code and start on fixing parser
    Fetch python files code would crash if no JSON was
    returned; fixed that, also added status udpate messages
    for number successful and failed retrievals. Started
    a python file to do a simple parse of the CFG grammar
    and also updating the grammar to align with the NLTK
    specifications.

--- data/fetch_python_files.py
+++ data/fetch_python_files.py
@@ -13,6 +13,8 @@ if not os.path.exists(PYTHON_FILES_DIR):
 
 API_BASE = "https://searchcode.com/api/result/"
 
+success = 0
+fail_ids = []
 lineNo = 0
 with open(IDS_STORE, "r") as f:
     for line in f:
@@ -22,16 +24,18 @@ with open(IDS_STORE, "r") as f:
         newdir = "{}/{}".format(PYTHON_FILES_DIR,str(l[0]))
         if not os.path.exists(newdir):
             url = API_BASE + "{}/".format(l[0])
-            response = requests.get(url)
-            data = json.loads(response.content)
             
-
             os.mkdir(newdir)
             newfile = "{}/{}".format(newdir,l[1])
             try:
+                response = requests.get(url)
+                data = json.loads(response.content)
+           
                 outfile = open(newfile, "w")
                 outfile.write(data["code"])
+                success = success + 1
             except:
+                fail_ids.append(l[0])
                 print "Failed to write code:"
                 print data["code"]
                 if os.path.exists(newfile):
@@ -40,3 +44,8 @@ with open(IDS_STORE, "r") as f:
             finally:
                 outfile.close()
         print "Resolved resource #{}".format(str(lineNo))
+print("{} files successfully retrieved, {} failures"
+      .format(success, len(fail_ids)))
+print("IDs of failed retrievals:")
+for fail in fail_ids:
+    print(" {}".format(fail))
---
+++ parse.py
@@ -0,0 +1,10 @@
+from nltk import CFG
+
+GRAMMAR_FILENAME = "cfg_grammar.txt"
+
+f = open(GRAMMAR_FILENAME, "r")
+file_contents_str = f.read()
+f.close()
+
+grammar = CFG.fromstring(file_contents_str)
+grammar.productions()
