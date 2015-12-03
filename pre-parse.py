73005e052044984b384ddc8a3992e7f5d7923f10
Jake Barnwell <jb16@mit.edu>
2015-12-03 17:47:44 -0500

    Adding this pre-parse thing, originally meant to help parse Strings and stuff

---
+++ pre-parse.py
@@ -0,0 +1,12 @@
+
+def findSTRING():
+	rSource = source.reverse()
+	regex = "'.*"
+	regex: '.*+'
+	(?<!#)(\s+commented) negative lookbehind
+
+	# accept: ... 'asdf' ...
+	# do not accept ... 'asdf\'ra' ...
+	# reversed: ... 'ar'\fdsa' 
+	# look for '.*'(?!\\)
+	# and for ".*"(?!\\)
